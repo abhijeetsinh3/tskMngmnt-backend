555f6d5069bbfdc844daa404ebfa94594f8eb419
Abhijeetsinh <90785910+abhijeetsinh3@users.noreply.github.com>
2025-03-28 13:10:12 +0530

    updated changes server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+module.exports = server; // Export the server instance
